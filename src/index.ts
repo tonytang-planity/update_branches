4cc5af45e489515bf22aec330340eefb96a55d4a
tonytang-planity <tony.tang@planity.com>
2023-01-14 18:23:07 +0100

    better typing

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,39 @@
 import repos from './data/repos.json'
 import { SimpleGit, simpleGit } from 'simple-git'
+import { Branch } from 'types/branch'
+import { Entries, PartialDeep } from 'type-fest'
+
+type Repos = Entries<typeof repos>
 
 // Change to true to debug
 const debug = false
 
 const MAGENTA = '\x1b[35m%s\x1b[0m'
+const RED = '\x1b[31m%s\x1b[0m'
 
 async function main() {
-    const features = Object.entries(repos)
+    const features = Object.entries(repos) as Repos
+
+    const branchesToUpdate = await pullBranchesAndGetNbCommitsBehind(features)
+
+    console.log({
+        branchesToUpdate: JSON.stringify(branchesToUpdate, null, ' '),
+    })
+}
+
+main()
+
+async function pullBranchesAndGetNbCommitsBehind(features: Repos) {
+    let branchesToUpdate: PartialDeep<
+        typeof repos,
+        { recurseIntoArrays: true }
+    > = {}
 
     for await (const [featureName, feature] of features) {
         console.log(`Feature: ${featureName}`)
 
-        const apps = Object.entries(feature)
+        const apps = Object.entries(feature) as Entries<typeof feature>
+
         for await (const [appName, app] of apps) {
             console.log(`[${featureName}] - App: ${appName}`)
 
@@ -41,8 +62,31 @@ async function main() {
                         MAGENTA,
                         `[${featureName}][${appName}][${left}...${right}] - ${nbCommitsBehind} commit(s) behind`
                     )
+
+                    if (nbCommitsBehind > 0) {
+                        const branchToUpdate: Branch = { left, right }
+
+                        if (!branchesToUpdate?.[featureName]?.[appName]) {
+                            // branches do not exists
+                            branchesToUpdate = {
+                                ...branchesToUpdate,
+                                [featureName]: {
+                                    [appName]: {
+                                        workdir,
+                                        branches: [branchToUpdate],
+                                    },
+                                },
+                            }
+                        } else {
+                            // branches array already exists
+                            branchesToUpdate?.[featureName]?.[
+                                appName
+                            ]?.branches?.push(branchToUpdate)
+                        }
+                    }
                 } catch (e) {
                     console.error(
+                        RED,
                         `[${featureName}][${appName}] - Comparing ${left} with ${right}`
                     )
                     console.error(e)
@@ -50,9 +94,9 @@ async function main() {
             }
         }
     }
-}
 
-main()
+    return branchesToUpdate
+}
 
 async function getNbCommitsBehind(
     git: SimpleGit,
---
+++ types/branch.d.ts
@@ -0,0 +1,4 @@
+export type Branch = {
+    left: string
+    right: string
+}
